62729a38a57c876227ca67be38d6f4e3cf162345
Nikita <nikita241296@gmail.com>
2018-02-27 17:10:30 +0200

    moxy and di added

--- app/src/main/java/com/example/nikita/upgameskotlin/BaseActivity.kt
+++ app/src/main/java/com/example/nikita/upgameskotlin/BaseActivity.kt
@@ -1,13 +1,13 @@
 package com.example.nikita.upgameskotlin
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import butterknife.ButterKnife
+import com.arellomobile.mvp.MvpActivity
 
 /**
  * Created by nikita on 27.02.2018.
  */
-open class BaseActivity : AppCompatActivity(){
+open class BaseActivity : MvpActivity(){
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     ButterKnife.bind(this)
