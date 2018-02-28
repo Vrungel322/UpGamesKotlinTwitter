cde0aae66562f0258910215f563f0014e0f7b865
Nikita <nikita241296@gmail.com>
2018-02-28 16:29:41 +0200

    send post button added

--- app/src/main/java/com/example/nikita/upgameskotlin/main/MainActivity.kt
+++ app/src/main/java/com/example/nikita/upgameskotlin/main/MainActivity.kt
@@ -3,9 +3,9 @@ package com.example.nikita.upgameskotlin.main
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import android.widget.Toast
 import butterknife.BindView
+import butterknife.OnClick
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.example.nikita.upgameskotlin.R.id
 import com.example.nikita.upgameskotlin.R.layout
@@ -16,22 +16,19 @@ import com.twitter.sdk.android.core.TwitterException
 import com.twitter.sdk.android.core.TwitterSession
 import com.twitter.sdk.android.core.identity.TwitterAuthClient
 import com.twitter.sdk.android.core.identity.TwitterLoginButton
-import io.reactivex.Observable
+import com.twitter.sdk.android.tweetcomposer.TweetComposer
 import timber.log.Timber
 
 
 
 class MainActivity : BaseActivity(), IMainActivityView {
   @InjectPresenter lateinit var mPresenter: MainActivityPresenter
-  @BindView(id.tvHelloWorld) lateinit var tvHelloWorld: TextView
   @BindView(id.tlbLogin) lateinit var twitterButton: TwitterLoginButton
 
   /* вопросом помечаем, что может прийти null */
   override fun onCreate(savedInstanceState: Bundle?) {
     setContentView(layout.activity_main)
     super.onCreate(savedInstanceState)
-
-    Observable.just("MainActivity kotlin Observable").subscribe({ Timber.e(it) })
 
     twitterButton.callback = object : Callback<TwitterSession>() {
       override fun success(result: Result<TwitterSession>) {
@@ -44,6 +41,10 @@ class MainActivity : BaseActivity(), IMainActivityView {
 
       }
     }
+  }
+
+  @OnClick(id.bSendPost) fun tvHelloWorldClicked(){
+    TweetComposer.Builder(applicationContext).show()
   }
 
   private fun requestEmailAddress(context: Context, session: TwitterSession) {
