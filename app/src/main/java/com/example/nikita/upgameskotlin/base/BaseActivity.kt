427110e5fc420f5be6ab5b9dca0ffca174edcfa4
Nikita <nikita241296@gmail.com>
2018-03-05 18:07:13 +0200

    remove BN

--- app/src/main/java/com/example/nikita/upgameskotlin/base/BaseActivity.kt
+++ app/src/main/java/com/example/nikita/upgameskotlin/base/BaseActivity.kt
@@ -1,17 +1,16 @@
 package com.example.nikita.upgameskotlin.base
 
 import android.os.Bundle
-import butterknife.ButterKnife
 import com.arellomobile.mvp.MvpActivity
 
 /**
  * Created by nikita on 27.02.2018.
  */
-abstract class BaseActivity : MvpActivity(){
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    ButterKnife.bind(this)
-    setUpUi()
-  }
-  abstract fun setUpUi()
+abstract class BaseActivity : MvpActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setUpUi()
+    }
+
+    abstract fun setUpUi()
 }
