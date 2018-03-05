07c86e6b0ee9421159b815f06a63fa87f4c80a30
Nikita <nikita241296@gmail.com>
2018-03-05 18:05:24 +0200

    open timeline

--- app/src/main/java/com/example/nikita/upgameskotlin/main/MainActivity.kt
+++ app/src/main/java/com/example/nikita/upgameskotlin/main/MainActivity.kt
@@ -3,10 +3,7 @@ package com.example.nikita.upgameskotlin.main
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
-import butterknife.BindView
-import butterknife.OnClick
 import com.arellomobile.mvp.presenter.InjectPresenter
-import com.example.nikita.upgameskotlin.R.id
 import com.example.nikita.upgameskotlin.R.layout
 import com.example.nikita.upgameskotlin.base.BaseActivity
 import com.example.nikita.upgameskotlin.timeline.TimeLineActivity
@@ -14,45 +11,39 @@ import com.twitter.sdk.android.core.Callback
 import com.twitter.sdk.android.core.Result
 import com.twitter.sdk.android.core.TwitterException
 import com.twitter.sdk.android.core.TwitterSession
-import com.twitter.sdk.android.core.identity.TwitterLoginButton
-import com.twitter.sdk.android.tweetcomposer.TweetComposer
+import kotlinx.android.synthetic.main.activity_main.tlbLogin
 import timber.log.Timber
 
 
 class MainActivity : BaseActivity(), IMainActivityView {
   @InjectPresenter lateinit var mPresenter: MainActivityPresenter
 
-  @BindView(id.tlbLogin) lateinit var twitterButton: TwitterLoginButton
-  /* вопросом помечаем, что может прийти null */
   override fun onCreate(savedInstanceState: Bundle?) {
     setContentView(layout.activity_main)
     super.onCreate(savedInstanceState)
   }
 
   override fun setUpUi() {
-    twitterButton.callback = object : Callback<TwitterSession>() {
+    tlbLogin.callback = object : Callback<TwitterSession>() {
       override fun success(result: Result<TwitterSession>) {
-        Timber.e("twitter success")
+        Timber.e("twitter success name: " + result.data.userName)
+        mPresenter.fillUser(result.data.userName)
         mPresenter.requestEmailAddress(result.data)
+        startActivity(Intent(this@MainActivity, TimeLineActivity::class.java))
+
       }
 
       override fun failure(exception: TwitterException) {
         Timber.e("twitter failure")
         Timber.e("twitter failure " + exception.localizedMessage)
-
       }
     }
   }
 
-  @OnClick(id.bSendPost)
-  fun bSendPostClicked() {
-    TweetComposer.Builder(this).show()
-  }
-
-  @OnClick(id.bTimeLine)
-  fun bTimeLineClick() {
-    startActivity(Intent(this@MainActivity, TimeLineActivity::class.java))
-  }
+//  @OnClick(id.bSendPost)
+//  fun bSendPostClicked() {
+//    TweetComposer.Builder(this).show()
+//  }
 
   override fun showToast(data: String?) {
     Toast.makeText(applicationContext, data, Toast.LENGTH_SHORT).show()
@@ -60,6 +51,6 @@ class MainActivity : BaseActivity(), IMainActivityView {
 
   override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
     super.onActivityResult(requestCode, resultCode, data)
-    twitterButton.onActivityResult(requestCode, resultCode, data)
+    tlbLogin.onActivityResult(requestCode, resultCode, data)
   }
 }
