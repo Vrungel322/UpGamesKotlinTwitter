085cb101a1a3ca9a75790a6a9a974c993be063b9
Nikita <nikita241296@gmail.com>
2018-03-05 17:55:26 +0200

    show user timeline

--- app/src/main/java/com/example/nikita/upgameskotlin/timeline/TimeLineActivity.kt
+++ app/src/main/java/com/example/nikita/upgameskotlin/timeline/TimeLineActivity.kt
@@ -2,45 +2,34 @@ package com.example.nikita.upgameskotlin.timeline
 
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.text.Editable
-import android.widget.EditText
-import butterknife.BindView
-import butterknife.OnTextChanged
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.example.nikita.upgameskotlin.R
 import com.example.nikita.upgameskotlin.base.BaseActivity
-import com.twitter.sdk.android.tweetui.SearchTimeline
 import com.twitter.sdk.android.tweetui.TweetTimelineRecyclerViewAdapter
+import com.twitter.sdk.android.tweetui.UserTimeline
+import kotlinx.android.synthetic.main.activity_time_line.rvTimeLine
+
 
 class TimeLineActivity : BaseActivity(), ITimeLineActivityView {
-  @InjectPresenter lateinit var mPresenter: TimeLineActivityPresenter
+    @InjectPresenter lateinit var mPresenter: TimeLineActivityPresenter
 
-  @BindView(R.id.rvTimeLine) lateinit var mRecycleViewTimeLine: RecyclerView
-  @BindView(R.id.etTimeLineQuery) lateinit var mEditTextTimeLineQuery: EditText
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setContentView(R.layout.activity_time_line)
+        super.onCreate(savedInstanceState)
+    }
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    setContentView(R.layout.activity_time_line)
-    super.onCreate(savedInstanceState)
-  }
+    override fun setUpUi() {
+        rvTimeLine.layoutManager = LinearLayoutManager(applicationContext)
 
-  override fun setUpUi() {
-    mRecycleViewTimeLine.layoutManager = LinearLayoutManager(applicationContext)
-  }
+        val userTimeline = UserTimeline.Builder()
+                .build()
 
-  @OnTextChanged(value = R.id.etTimeLineQuery, callback = OnTextChanged.Callback.AFTER_TEXT_CHANGED)
-  fun updateTimeLine(s: Editable) {
+        val adapter = TweetTimelineRecyclerViewAdapter.Builder(applicationContext)
+                .setTimeline(userTimeline)
+                .setViewStyle(R.style.tw__TweetLightWithActionsStyle)
+                .build()
 
-    val searchTimeline = SearchTimeline.Builder()
-        .query("#"+s.toString())
-        .maxItemsPerRequest(50)
-        .build()
+        rvTimeLine.adapter = adapter
+    }
 
-    val adapter = TweetTimelineRecyclerViewAdapter.Builder(applicationContext)
-        .setTimeline(searchTimeline)
-        .setViewStyle(R.style.tw__TweetLightWithActionsStyle)
-        .build()
-
-    mRecycleViewTimeLine.adapter = adapter
-  }
 }
